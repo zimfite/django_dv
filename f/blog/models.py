676d41754f116d54c3662dbfdc64081197516328
zimfite <katy2007dv@gmail.com>
2023-12-17 22:01:17 +0300

    add

--- f/blog/models.py
+++ f/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField("Название",  max_length=50)
+    post = models.TextField("Описание")
+
+    def __str__(self):
+        return self.title
--- f/blog/views.py
+++ f/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from.models import Post
 # Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html' ,
+                  {"posts": posts,
+                  "title": "Home"})
+
+def about(request):
+    return render(request, 'blog/about.html')
